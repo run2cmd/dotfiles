9be997e9c55dabd7e0e1c95a93c166d506e7d47e
Bugala Piotr <Piotr.Bugala@igt.com>
2020-06-09 09:54:25 +0200

    Create docker for jenkins lint on the fly

--- .vim/ftplugin/Jenkinsfile.vim
+++ .vim/ftplugin/Jenkinsfile.vim
@@ -1,7 +1,5 @@
 setlocal tabstop=4 shiftwidth=4
 
 if has('win32')
-  let b:dispatch = $HOME . "\\scripts\\jlint.bat % " . g:jenkins_user . " " . g:jenkins_node . " " . g:jenkins_port
-else
-  let b:dispatch = "ssh -o StrictHostKeyChecking=no -l " . g:jenkins_user . " " . g:jenkins_node . "-p " . g:jenkins_port . " declarative-linter < %"
+  let b:dispatch = $HOME . "\\scripts\\jlint.bat %"
 endif
--- scripts/jlint.bat
+++ scripts/jlint.bat
@@ -1,2 +1,8 @@
 @echo off
-type %1 | ssh -l %2 -o StrictHostKeyChecking=no %3 -p %4 declarative-linter
+for /f %%i in ('docker run -d -p 52222:52222 jenkins-env') do set dockerid=%%i
+ssh-keygen -R [localhost]:52222 > nul
+echo "Waiting 30 seconds for Jenkins on docker to come up"
+timeout 30 > nul
+type %1 | ssh -l admin -o StrictHostKeyChecking=no localhost -p 52222 declarative-linter
+docker kill %dockerid% > nul
+docker rm %dockerid% > nul
