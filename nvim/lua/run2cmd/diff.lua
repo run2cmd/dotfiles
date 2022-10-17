0bb0f38f497a15f3e2d8ec49420b537230e155c8
Bugala Piotr <Piotr.Bugala@igt.com>
2022-10-17 09:28:47 +0200

    Add wrap to Neovim diff

--- nvim/lua/run2cmd/diff.lua
+++ nvim/lua/run2cmd/diff.lua
@@ -1,6 +1,6 @@
 --
 -- Diff mode
 --
-vim.o.diffopt = vim.o.diffopt .. ',vertical'
+vim.o.diffopt = vim.o.diffopt .. ',vertical,followwrap'
 vim.g.ZFDirDiffFileExclude = 'CVS,.git,.svn'
 vim.g.ZFDirDiffShowSameFile = 0
