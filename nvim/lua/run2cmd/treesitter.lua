93336b9fe245e678788df748e91a5e82674e5c54
Bugala Piotr <Piotr.Bugala@igt.com>
2022-10-17 09:31:56 +0200

    Disable treesitter for vim help.

--- nvim/lua/run2cmd/treesitter.lua
+++ nvim/lua/run2cmd/treesitter.lua
@@ -1,11 +1,25 @@
 local config = require('nvim-treesitter.configs')
+--local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+-- Additional language support
+-- parser_config.puppet = {
+--   install_info = {
+--     url = '~/tools/tree-sitter-puppet',
+--     files = { 'src/parser.c' },
+--     branch = 'main',
+--     requires_generate_from_grammar = false,
+--     generate_requires_npm = false,
+--   },
+-- }
 
 -- Enable treesitter
 config.setup({
   ensure_installed = 'all',
+  --ignore_install = { 'markdown_inline' },
   sync_install = false,
   highlight = {
     enable = true,
+    disable = { 'help' },
     additional_vim_regex_highlighting = false,
   },
   endwise = {
