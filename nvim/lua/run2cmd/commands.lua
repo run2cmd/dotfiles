cf25358719be7c40df61ec69feb218af775cb893
Bugala Piotr <Piotr.Bugala@igt.com>
2022-11-04 07:10:18 +0100

    nvim: Todo in nice split

--- nvim/lua/run2cmd/commands.lua
+++ nvim/lua/run2cmd/commands.lua
@@ -3,7 +3,7 @@
 --
 local cmd = vim.api.nvim_create_user_command
 
-cmd('Todo', ":e ~/notes.md", {})
+cmd('Todo', ":split ~/notes.md", {})
 cmd('Config', ':e ~/dotfiles/nvim/init.lua', {})
 cmd('Doc', ":lua require('run2cmd.helper-functions').chtsh('<args>')", { nargs = '*' })
 cmd('Terminal', ":lua require('run2cmd.helper-functions').run_term_cmd('<args>')", { nargs = '*'})
