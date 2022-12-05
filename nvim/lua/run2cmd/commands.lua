1898144cacd1259de4a6f2f6c5fed247968947be
Bugala Piotr <Piotr.Bugala@igt.com>
2022-12-05 09:41:30 +0100

    nvim: Config opens README by default

--- nvim/lua/run2cmd/commands.lua
+++ nvim/lua/run2cmd/commands.lua
@@ -3,7 +3,6 @@
 --
 local cmd = vim.api.nvim_create_user_command
 
-cmd('Todo', ":split ~/notes.md", {})
-cmd('Config', ':e ~/dotfiles/nvim/init.lua', {})
+cmd('Config', ':e ~/dotfiles/README.md', {})
 cmd('Doc', ":lua require('run2cmd.helper-functions').chtsh('<args>')", { nargs = '*' })
 cmd('Terminal', ":lua require('run2cmd.helper-functions').run_term_cmd('<args>')", { nargs = '*'})
