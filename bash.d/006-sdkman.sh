642afc9b992bda4ab4077f0aee97477fda54cfb8
Bugala Piotr <Piotr.Bugala@igt.com>
2023-04-28 09:07:30 +0200

    feat(java): Performance settings.

--- bash.d/006-sdkman.sh
+++ bash.d/006-sdkman.sh
@@ -4,3 +4,6 @@ export SDKMAN_DIR="$HOME/.sdkman"
 
 # Never run gradle daemon. For multiple project causes perfomrance issues.
 export GRADLE_OPTS=-Dorg.gradle.daemon=false
+
+# Limit JAVA to not kill OS
+export JAVA_OPTS='-Xms256m -Xmx2048m'
