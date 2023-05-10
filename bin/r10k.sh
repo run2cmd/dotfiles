f0c4c19316a3a9c81c2a1ecd5291bdddcc55dd3b
Bugala Piotr <Piotr.Bugala@igt.com>
2023-05-10 18:11:27 +0200

    feat(r10k): Switch to Ansible to run on muliple nodes

--- bin/r10k.sh
+++ bin/r10k.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-PUPPET_MASTER_IP=$(rg -I puppet_master_ip: |head -1 | awk '{print $2}' | sed "s/'//g;s/\"//g")
+PUPPET_MASTER_IPS=$(rg -I puppet_master_ip: | awk '{print $2}' | sed "s/'//g;s/\"//g" | tr -s "\n" ",")
 ENV_NAME="$(basename $(pwd))_$(git branch --show-current)"
 
-echo "Run r10k with environment ${ENV_NAME} on ${PUPPET_MASTER_IP}"
-echo "${PUPPET_MASTER_IP} \"r10k deploy environment ${ENV_NAME} -pv\""
-
-ssh ${PUPPET_MASTER_IP} "r10k deploy environment ${ENV_NAME} -pv"
+echo "Run r10k with environment ${ENV_NAME} on ${PUPPET_MASTER_IPS}"
+ansible-playbook -i $PUPPET_MASTER_IPS -u root /dev/stdin <<FOO
+- hosts: all
+  gather_facts: false
+  tasks:
+    - command: "r10k deploy environment ${ENV_NAME} -pv"
+FOO
