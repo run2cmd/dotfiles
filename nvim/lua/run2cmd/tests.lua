06e69a93545f1e83d5e15be6b2c62e2f4dddf142
Bugala Piotr <Piotr.Bugala@igt.com>
2023-05-10 08:33:38 +0200

    feat(nvim): Simplified testing suite.

--- nvim/lua/run2cmd/tests.lua
+++ nvim/lua/run2cmd/tests.lua
@@ -6,146 +6,126 @@ local mapkey = vim.keymap.set
 local ruby_env = 'source ~/.rvm/scripts/rvm && rvm use'
 
 local test_tbl = {
-  projects = {
-    maven = {
-      marker = 'pom.xml',
-      command = 'mvn clean install',
-    },
-    nodejs = {
-      marker = 'package.json',
-      command = 'yarn install & yarn build:prod',
-    },
-    icha = {
-      marker = 'Puppetfile',
-      command = 'ichatest.sh',
-    },
-    gradle = {
-      marker = 'build.gradle',
-      command = '$NVIM_GRADLE_BIN clean build --info',
-    },
-    helm = {
-      marker = 'helm',
-      command = 'for i in $(ls helm) ;do mkdir -p templates_out/${i} && helm template helm/${i} --output-dir templates_out ;done',
-    },
-    ruby = {
-      marker = 'Gemfile',
-      exclude = 'Puppetfile',
-      setup = ruby_env .. '&& bundle install && bundle exec rake spec_prep',
-      command = ruby_env .. '&& bundle exec rake spec',
-      errors = 'Error',
-    },
+  maven = {
+    rootfile = 'pom.xml',
+    command = 'mvn clean install',
   },
-  file_types = {
-    groovy = {
-      command = 'groovy %',
-      alternatives = {
-        {
-          filename_contain = 'Test.groovy',
-          command = '$NVIM_GRADLE_BIN clean test --tests %:t:r --info',
-        },
-      },
-    },
-    ruby = {
-      command = ruby_env .. '&& ruby %',
-      alternatives = {
-        {
-          filename_contain = '_spec.rb',
-          command = ruby_env .. '&& BEAKER_destroy=no bundle exec rspec %',
-        },
-      },
-    },
-    plantuml = {
-      command = 'plantuml -tsvg -o ' .. vim.env.HOME .. '/.config/nvim/tmp %',
-    },
-    python = {
-      command = 'python %',
-    },
-    puppet = {
-      -- Use ruby 2.4.10 with Puppet 5.5.22
-      command = ruby_env .. ' 2.4.10 && puppet apply --noop %',
-    },
-    sh = {
-      command = 'bash %',
-    },
-    xml = {
-      command = 'mvn clean install -f %',
-    },
-    lua = {
-      command = 'lua %',
-    },
+  nodejs = {
+    rootfile = 'package.json',
+    command = 'yarn install & yarn build:prod',
+  },
+  icha = {
+    rootfile = 'Puppetfile',
+    command = 'ichatest.sh',
+  },
+  gradle = {
+    rootfile = 'build.gradle',
+    command = '$NVIM_GRADLE_BIN clean build --info',
+  },
+  helm = {
+    rootfile = 'helm',
+    command = 'for i in $(ls helm) ;do mkdir -p templates_out/${i} && helm template helm/${i} --output-dir templates_out ;done',
+  },
+  ruby_proj = {
+    rootfile =  'Gemfile',
+    exclude = 'Puppetfile',
+    setup = ruby_env .. '&& bundle install && bundle exec rake spec_prep',
+    command = ruby_env .. '&& bundle exec rake spec',
+    errors = 'Error',
+  },
+  groovy = {
+    command = 'groovy %',
+  },
+  groovy_test = {
+    pattern = 'Test.groovy',
+    command = '$NVIM_GRADLE_BIN clean test --tests %:t:r --info',
+  },
+  ruby = {
+    command = ruby_env .. '&& ruby %',
+  },
+  ruby_spec = {
+    pattern = '_spec.rb',
+    command = ruby_env .. '&& BEAKER_destroy=no bundle exec rspec %',
+  },
+  plantuml = {
+    command = 'plantuml -tsvg -o ' .. vim.env.HOME .. '/.config/nvim/tmp %',
+  },
+  python = {
+    command = 'python %',
+  },
+  puppet = {
+    -- Use ruby 2.4.10 with Puppet 5.5.22
+    command = ruby_env .. ' 2.4.10 && puppet apply --noop %',
+  },
+  sh = {
+    command = 'bash %',
+  },
+  xml = {
+    command = 'mvn clean install -f %',
+  },
+  lua = {
+    command = 'lua %',
   },
 }
 
+
+--
+-- Find and parse test data.
+--
+-- @param setter filename pattern to compare against test_tbl.
+--
+-- @return { cmd = String, err = String, proj = String, prep = String }
+--
+local function find_test(setter)
+  local data = {}
+
+  for k, v in pairs(test_tbl) do
+    local filepattern = v.pattern or k
+    local filemark = v.rootfile or 'none'
+    local exclude = v.exclude or 'none'
+
+    if string.match(setter, filepattern) then
+      data.cmd = v.command
+      data.err = v.errors or 'FAILED'
+    elseif helpers.file_exists(filemark) and not helpers.file_exists(exclude) then
+      data.proj = v.command
+      data.prep = v.setup or 'none'
+      data.err = v.errors or 'FAILED'
+    end
+  end
+
+  return data
+end
+
+-- Run current file test
 local function run_file()
-  local filetype = vim.bo.filetype
-  local filename = vim.fn.expand('%:t')
-  local test_config = test_tbl.file_types[filetype]
-  local test_cmd = test_config.command
-  local error_str = test_config.errors or 'FAILED'
-  local alts = test_config.alternatives or {}
-
-  for _, t in pairs(alts) do
-    if string.match(filename, t.filename_contain) then
-      test_cmd = t.command
-      error_str = t.errors or 'FAILED'
-      break
-    end
-  end
-
-  vim.g.term_error_serach_string = error_str
-  helpers.run_term_cmd(test_cmd)
+  local test_data = helpers.merge(find_test(vim.bo.filetype), find_test(vim.fn.expand('%:t')))
+  vim.g.term_error_serach_string = test_data.err
+  helpers.run_term_cmd(test_data.cmd)
 end
 
+-- Run project test
 local function run_project()
-  local project_marks = test_tbl.projects
-  local error_str
-  local test_cmd
-
-  for _, v in pairs(project_marks) do
-    if helpers.file_exists(v.marker) then
-      local exclude = v.exclude or 'no_exclude_file_check'
-
-      if not helpers.file_exists(exclude) then
-        test_cmd = v.command
-        error_str = v.errors or 'FAILED'
-        break
-      end
-    end
-  end
-
-  vim.g.term_error_serach_string = error_str
-  helpers.run_term_cmd(test_cmd)
+  local test_data = find_test('project')
+  vim.g.term_error_serach_string = test_data.err
+  helpers.run_term_cmd(test_data.proj)
 end
 
+-- Rerun last test
 local function run_last()
   helpers.run_term_cmd(vim.g.last_terminal_test)
 end
 
+--- Find errors in test window
 local function find_errors()
   vim.cmd('/' .. vim.g.term_error_serach_string)
 end
 
+-- Run project preparation steps
 local function run_setup()
-  local project_marks = test_tbl.projects
-  local error_str
-  local test_cmd
-
-  for _, v in pairs(project_marks) do
-    if helpers.file_exists(v.marker) then
-      local exclude = v.exclude or 'no_exclude_file_check'
-
-      if not helpers.file_exists(exclude) then
-        test_cmd = v.setup or 'none'
-        error_str = v.errors or 'FAILED'
-        break
-      end
-    end
-  end
-
-  vim.g.term_error_serach_string = error_str
-  if not(test_cmd == 'none') then
-    helpers.run_term_cmd(test_cmd)
-  end
+  local test_data = find_test('project')
+  vim.g.term_error_serach_string = test_data.err
+  helpers.run_term_cmd(test_data.prep)
 end
 
 -- Easy mappings for for running tests. Got used to vim-dispatch in past so use them.
