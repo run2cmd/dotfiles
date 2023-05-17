903a52968c7b3f058b40465727276bccfb0c1daa
Bugala Piotr <Piotr.Bugala@igt.com>
2023-05-17 09:37:02 +0200

    feat(r10k): Multi puppet master support.

--- bin/r10k.sh
+++ bin/r10k.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-PUPPET_MASTER_IPS=$(rg -I puppet_master_ip: | awk '{print $2}' | sed "s/'//g;s/\"//g" | tr -s "\n" ",")
 ENV_NAME="$(basename $(pwd))_$(git branch --show-current)"
+LOCATIONS=$(python3 -c "import yaml;print(','.join(list(yaml.safe_load(open('data/env/${ENV_NAME}.yaml'))['icha_infrastructure'].keys())))")
 
-echo "Run r10k with environment ${ENV_NAME} on ${PUPPET_MASTER_IPS}"
+PUPPET_MASTER_IPS=$(rg -I --glob "{${LOCATIONS}}.yaml" puppet_master_ip: | awk '{print $2}' | sed "s/'//g;s/\"//g" | tr -s "\n" ",")
+
+echo "Run r10k with environment ${ENV_NAME} in locations ${LOCATIONS} on ${PUPPET_MASTER_IPS}"
 ansible-playbook -i $PUPPET_MASTER_IPS -u root /dev/stdin <<FOO
 - hosts: all
   gather_facts: false
