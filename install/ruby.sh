c886c274ee3a82ed08f3c19d2c1e6cde4547880b
Bugala Piotr <Piotr.Bugala@igt.com>
2023-07-20 09:59:27 +0200

    feat(ruby): Fix ruby SSL issues.

--- install/ruby.sh
+++ install/ruby.sh
@@ -7,12 +7,12 @@ source ${LIBDIR}/lib.sh
 
 topic 'UPDATE RUBY'
 
-RUBY_VERSION=3.2.2
+DEFAULT_RUBY=3.2.2
 
+task 'Update rvm'
 if [ ! -e ${HOME}/.rvm ] ;then
   curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
-  curl -sSL https://get.rvm.io | bash -s stable --version $RUBY_VERSION
-  source '/home/pbugala/.rvm/scripts/rvm'
+  curl -sSL https://get.rvm.io | bash -s stable --version $DEFAULT_RUBY
 fi
 
 rvm get stable
@@ -23,9 +23,18 @@ if [ ! -e ${rvm_path}/usr/bin/openssl ] ;then
   rvm pkg install openssl
 fi
 
-rvm install $RUBY_VERSION --default
+source "${HOME}/.rvm/scripts/rvm"
+
+task 'Install rubies'
+rvm install $DEFAULT_RUBY --default
 rvm install 2.7.3 --with-openssl-dir=$HOME/.rvm/usr
 rvm install 2.4.10 --with-openssl-dir=$HOME/.rvm/usr
 
+# Install rubocop per ruby version for solargraph proper support
+rvm use 2.7.3 && (which rubocop &>/dev/null|| gem install rubocop --version 1.54.1)
+rvm use 2.4.10 && (which rubocop &>/dev/null || gem install rubocop --version 0.93.1)
+
+task "Update default ruby"
+rvm use $DEFAULT_RUBY
 gem install bundle
 bundle update
