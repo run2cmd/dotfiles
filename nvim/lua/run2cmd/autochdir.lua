bff07138306c69f5fcc8010af3fcbc9f714cc48f
Bugala Piotr <Piotr.Bugala@igt.com>
2023-07-20 10:01:45 +0200

    feat(nvim): Autochdir should use file dirname() in case no root marker found.

--- nvim/lua/run2cmd/autochdir.lua
+++ nvim/lua/run2cmd/autochdir.lua
@@ -5,9 +5,9 @@ local cache = {}
 
 -- Find project root directory
 local function find_root(buf_id, names)
-  local path = vim.api.nvim_buf_get_name(buf_id)
-  if path == '' then return end
-  path = vim.fs.dirname(path)
+  local file_path = vim.api.nvim_buf_get_name(buf_id)
+  if file_path == '' then return end
+  local path = vim.fs.dirname(file_path)
 
   -- Use cache
   local result = cache[path]
@@ -15,7 +15,13 @@ local function find_root(buf_id, names)
 
   -- Find root
   local root_file = vim.fs.find(names, { path = path, upward = true })[1]
-  if root_file == nil then return end
+  if root_file == nil then
+    if vim.api.nvim_get_option_value('filetype', {buf=buf_id}) ~= '' then
+      root_file = file_path
+    else
+      return
+    end
+  end
 
   -- Exclude dir name
   if string.match(root_file, path_exclude) then return end
@@ -30,7 +36,7 @@ local function find_root(buf_id, names)
 end
 
  -- Change directory
- local function set_root()
+local function set_root()
   local root = find_root(0, dir_names)
   if root == nil then return end
   vim.fn.chdir(root)
