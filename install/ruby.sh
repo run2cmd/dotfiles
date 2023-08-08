09cb4bf7ff09c06f4eca977e17f4eb2ed2747bbd
Bugala Piotr <Piotr.Bugala@igt.com>
2023-08-08 07:35:52 +0200

    feat(ruby): Add proper certs directory for rvm openssl

--- install/ruby.sh
+++ install/ruby.sh
@@ -15,6 +15,8 @@ if [ ! -e ${HOME}/.rvm ] ;then
   curl -sSL https://get.rvm.io | bash -s stable --version $DEFAULT_RUBY
 fi
 
+source "${HOME}/.rvm/scripts/rvm"
+
 rvm get stable
 rvm autolibs enable
 
@@ -23,12 +25,18 @@ if [ ! -e ${rvm_path}/usr/bin/openssl ] ;then
   rvm pkg install openssl
 fi
 
+# Add proper certificates for older OpenSSL
+if [ ! -L "${rvm_path}/usr/ssl" ] ;then
+  mv ${rvm_path}/usr/ssl ${rvm_path}/usr/ssl_orig
+  ln -s /usr/lib/ssl ${rvm_path}/usr/ssl
+fi
+
 source "${HOME}/.rvm/scripts/rvm"
 
 task 'Install rubies'
 rvm install $DEFAULT_RUBY --default
-rvm install 2.7.3 --with-openssl-dir=$HOME/.rvm/usr
-rvm install 2.4.10 --with-openssl-dir=$HOME/.rvm/usr
+rvm install 2.7.3 --with-openssl-dir=${rvm_path}/usr
+rvm install 2.4.10 --with-openssl-dir=${rvm_path}/usr
 
 # Install rubocop per ruby version for solargraph proper support
 rvm use 2.7.3 && (which rubocop &>/dev/null|| gem install rubocop --version 1.54.1)
