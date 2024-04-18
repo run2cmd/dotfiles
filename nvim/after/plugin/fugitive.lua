24190f42ab1c4a3f1d670bcd02cc38e94eca5abb
Bugala Piotr <Piotr.Bugala@igt.com>
2024-04-18 08:14:34 +0200

    feat(nvim): Simplify git log

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,5 +1,4 @@
 local mapkey = vim.keymap.set
-local helpers = require('run2cmd.helper-functions')
 
 mapkey('n', '<leader>ge', ':Ggrep "^<<<<<"<CR>')
 
@@ -19,35 +18,6 @@ mapkey('n', '<leader>gdo', ':DiffviewOpen<cr>')
 mapkey('n', '<leader>gdc', ':DiffviewClose<cr>')
 mapkey('n', '<leader>gdr', ':DiffviewRefreash<cr>')
 
-mapkey('n', '<leader>gll', ':Git log --pretty=format:"%h %an %ad%d %s" --date=short<cr>')
+mapkey('n', '<leader>gll', ':Git log --pretty=format:"%h %an %ad%d %s" --date=short<cr><cmd>silent :ownsyntax gitlog<cr>')
 mapkey('n', '<leader>glp', ':vert Git log --decorate=full -p --full-diff<cr>')
 mapkey('n', '<leader>glf', ':vert Git log --decorate=full -p --follow -- %<cr>')
-
-local autocmds = {
-  git = {
-    { event = 'BufEnter', opts = { pattern = 'COMMIT_EDITMSG', command = 'startinsert' } },
-    { event = 'BufEnter',
-      opts = {
-        pattern = '*',
-        callback = function()
-          local current_buf = vim.api.nvim_get_current_buf()
-          if vim.api.nvim_get_option_value('filetype', {}) == 'git' then
-            local content = vim.api.nvim_buf_get_lines(current_buf, 1, 6, false)
-            local count = 0
-            for _, c in ipairs(content) do
-              local sub = string.sub(c, 1, 7)
-              if not string.match(sub, ' ') then
-                count = count + 1
-              end
-            end
-            if count == 5 then
-              vim.cmd('ownsyntax gitlog')
-            end
-          end
-        end
-      }
-    }
-  }
-}
-
-helpers.create_autocmds(autocmds)
