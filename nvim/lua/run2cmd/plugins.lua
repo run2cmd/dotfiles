8725d072f32ec7489d78b645c770bd15bdc26346
Bugala Piotr <Piotr.Bugala@igt.com>
2024-06-05 07:46:39 +0200

    feat(nvim): Add nvim tags for autocompletion.

--- nvim/lua/run2cmd/plugins.lua
+++ nvim/lua/run2cmd/plugins.lua
@@ -10,6 +10,7 @@ require("paq") {
   { 'hrsh7th/cmp-nvim-lsp' },
   { 'hrsh7th/cmp-buffer' },
   { 'hrsh7th/cmp-path' },
+  { 'quangnguyen30192/cmp-nvim-tags' },
   { 'hrsh7th/nvim-cmp' },
   { 'L3MON4D3/LuaSnip' },
   { 'saadparwaiz1/cmp_luasnip' },
