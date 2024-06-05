36fa730c35682e5cfedc8700a42b2c7a7a422b84
Bugala Piotr <Piotr.Bugala@igt.com>
2024-06-05 07:47:33 +0200

    feat(nvim): GitHub integration with PR and Workflow runs

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -9,14 +9,8 @@ local mapkey = vim.keymap.set
 
 local function get_selection(buffer)
   local table = action_state.get_selected_entry(buffer)
-  local path = table[1]
-  return path
-end
-
-local function open_project(buffer)
-  local dir_path = get_selection(buffer)
-  actions.close(buffer)
-  builtin.find_files({ cwd = string.gsub(dir_path, '/$', '') })
+  local item = table[1]
+  return item
 end
 
 local function open_float(buffer)
@@ -26,21 +20,18 @@ local function open_float(buffer)
   helper.float_buffer(file_path, opts)
 end
 
-local function open_buffer(buffer)
-  local file_path = get_selection(buffer)
-  actions.close(buffer)
-  vim.cmd.edit(file_path)
-end
-
 local function find_projects()
   pickers
     .new({}, {
-      prompt_title = 'projects',
+      prompt_title = 'Projects',
       finder = finders.new_oneshot_job({ 'fdfind', '--type=d', '--exact-depth=2', '--full-path', '.', '/code' }),
       sorter = config.values.generic_sorter({}),
       attach_mappings = function(_, map)
-        map('i', '<cr>', open_project)
-        map('n', '<cr>', open_project)
+        map({ 'i', 'n' }, '<cr>', function (buffer)
+          local dir_path = get_selection(buffer)
+          actions.close(buffer)
+          builtin.find_files({ cwd = string.gsub(dir_path, '/$', '') })
+        end)
         return true
       end,
     })
@@ -50,12 +41,62 @@ end
 local function find_notes()
   pickers
     .new({}, {
-      prompt_title = 'notes',
+      prompt_title = 'Notes',
       finder = finders.new_oneshot_job({ 'fdfind', '--full-path', '.', '/home/pbugala/.notes' }),
       sorter = config.values.generic_sorter({}),
       attach_mappings = function(_, map)
-        map('i', '<cr>', open_buffer)
-        map('n', '<cr>', open_buffer)
+        map({ 'i', 'n' }, '<cr>', function(buffer)
+          local file_path = get_selection(buffer)
+          actions.close(buffer)
+          vim.cmd.edit(file_path)
+        end)
+        return true
+      end,
+    })
+    :find()
+end
+
+local function find_gh_prs()
+  pickers
+    .new({}, {
+      prompt_title = 'GitHub Pull Requests',
+      finder = finders.new_oneshot_job({ 'gh', 'pr', 'list' }),
+      sorter = config.values.generic_sorter({}),
+      attach_mappings = function(_, map)
+        map({ 'i', 'n' }, '<cr>', function(buffer)
+          local selection = get_selection(buffer):match('%d+')
+          local pr_file = '/tmp/gh_pr.git'
+          vim.cmd('silent !gh pr view ' .. selection .. ' &> ' .. pr_file)
+          vim.cmd('silent !gh pr checks ' .. selection .. ' &>> ' .. pr_file)
+          vim.cmd('silent !gh pr diff ' .. selection .. ' &>> ' .. pr_file)
+          actions.close(buffer)
+          local opts = { width = 220, height = 50, anchor = 'NW', col = 10, row = 2, border = 'double' }
+          helper.float_buffer(pr_file, opts)
+          vim.opt_local.filetype = 'git'
+        end)
+        return true
+      end,
+    })
+    :find()
+end
+
+local function find_gh_runs()
+  pickers
+    .new({}, {
+      prompt_title = 'GitHub Workflow Runs',
+      finder = finders.new_oneshot_job({ 'gh', 'run', 'list' }),
+      sorter = config.values.generic_sorter({}),
+      attach_mappings = function(_, map)
+        map({ 'i', 'n' }, '<cr>', function(buffer)
+          local selection = get_selection(buffer):match('%d+%d+%d+%d+%d+')
+          local runs_file = '/tmp/gh_runs.git'
+          vim.cmd('silent !gh run view --log ' .. selection .. '| ansi2txt &> ' .. runs_file)
+          actions.close(buffer)
+          vim.cmd('vsplit')
+          vim.cmd.edit(runs_file)
+          vim.opt_local.list = false
+          vim.cmd('/warning\\|error\\|failed')
+        end)
         return true
       end,
     })
@@ -118,3 +159,5 @@ mapkey('n', '<C-n>', find_notes)
 mapkey('n', '<leader>sw', builtin.grep_string)
 mapkey('n', '<leader>sl', builtin.live_grep)
 mapkey('n', '<leader>sb', builtin.current_buffer_fuzzy_find)
+mapkey('n', '<leader>sp', find_gh_prs)
+mapkey('n', '<leader>sr', find_gh_runs)
