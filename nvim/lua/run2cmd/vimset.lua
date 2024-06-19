e36909ef858c1c11af5bcdd05e8c81ac7cfa9ab4
Bugala Piotr <Piotr.Bugala@igt.com>
2024-06-19 10:44:05 +0200

    feat(nvim): Remove file encoding. It is already set to utf8 by default

--- nvim/lua/run2cmd/vimset.lua
+++ nvim/lua/run2cmd/vimset.lua
@@ -17,10 +17,6 @@ vim.o.spelllang = 'en_us'
 vim.o.spellfile = vim.env.HOME .. '/.config/nvim/spell/en.utf8.add'
 vim.o.spell = false
 
-vim.go.fileencoding = 'utf-8'
-vim.o.encoding = 'utf-8'
-vim.o.fileencodings = 'utf-8'
-
 vim.o.completeopt = 'menu,menuone,noinsert,noselect'
 vim.o.shortmess = vim.o.shortmess .. 'cm'
 vim.o.complete = string.gsub(vim.o.complete, 't', '')
