81175c8122c5ee7bfaf477d95e1134b526986ac6
Bugala Piotr <Piotr.Bugala@igt.com>
2024-06-27 07:58:23 +0200

    feat(ssh): Remove unneccecary redirects from ssh_agent start

--- bash.d/001-ssh_agent.sh
+++ bash.d/001-ssh_agent.sh
@@ -2,17 +2,17 @@
 SSH_ENV="${HOME}/.ssh/environment"
 function start_agent {
   echo "Initialising new SSH agent..."
-  /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
+  /usr/bin/ssh-agent | grep export > "${SSH_ENV}"
   echo succeded
   chmod 600 "${SSH_ENV}"
-  . "${SSH_ENV}" > /dev/null
+  . "${SSH_ENV}"
   for key in ~/.ssh/id*[^.pub] ;do
     /usr/bin/ssh-add $key
   done
 }
 if [ -f "${SSH_ENV}" ]; then
-  . "${SSH_ENV}" > /dev/null
-  ps -ef | grep -i ssh| grep ${SSH_AGENT_PID} | grep ssh-agent &> /dev/null || { start_agent; }
+  . "${SSH_ENV}"
+  ps --pid ${SSH_AGENT_PID} > /dev/null || start_agent
 else
-  start_agent;
+  start_agent
 fi
