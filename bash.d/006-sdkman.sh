39da31cafa622850b384750b35dd82fc49635262
Bugala Piotr <Piotr.Bugala@igt.com>
2024-07-15 09:16:44 +0200

    feat(maven): Allo unsecured registires

--- bash.d/006-sdkman.sh
+++ bash.d/006-sdkman.sh
@@ -7,3 +7,6 @@ export GRADLE_OPTS=-Dorg.gradle.daemon=false
 
 # Limit JAVA to not kill OS
 export JAVA_OPTS='-Xms256m -Xmx2048m'
+
+# Allow broken certs for maven
+export MVN_ARGS='-Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true -Dmaven.resolver.transport=wagon'
