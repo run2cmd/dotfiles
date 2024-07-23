26e8d66a48f3d7463a7d8697e3d2803d20923387
Bugala Piotr <Piotr.Bugala@igt.com>
2024-07-23 08:17:24 +0200

    feat(nvim): Prettier function for autosave

--- nvim/lua/run2cmd/autosave.lua
+++ nvim/lua/run2cmd/autosave.lua
@@ -1,5 +1,24 @@
 local helpers = require('run2cmd.helper-functions')
 
+local function auto_save()
+  local buffer = vim.api.nvim_get_current_buf()
+  local excluded = { 'terminal', 'nofile', 'finished', 'gitcommit', 'startify' }
+
+  if vim.api.nvim_buf_get_name(buffer) ~= '' then
+    local modified = vim.api.nvim_buf_get_option(buffer, 'modified')
+    local filetype = vim.api.nvim_get_option_value('filetype', { buf = buffer })
+    local buftype = vim.api.nvim_get_option_value('buftype', { buf = buffer })
+
+    if modified then
+      if helpers.table_contains(excluded, filetype) or helpers.table_contains(excluded, buftype) then
+        return
+      else
+        vim.cmd('write')
+      end
+    end
+  end
+end
+
 local autocmds = {
   autosave = {
     {
@@ -7,22 +26,7 @@ local autocmds = {
       opts = {
         pattern = '*',
         callback = function()
-          local buffer = vim.api.nvim_get_current_buf()
-          local excluded = { 'terminal', 'nofile', 'finished', 'gitcommit', 'startify' }
-
-          if vim.api.nvim_buf_get_name(buffer) ~= '' then
-            local modified = vim.api.nvim_buf_get_option(buffer, 'modified')
-            local filetype = vim.api.nvim_get_option_value('filetype', { buf = buffer })
-            local buftype = vim.api.nvim_get_option_value('buftype', { buf = buffer })
-
-            if modified then
-              if helpers.table_contains(excluded, filetype) or helpers.table_contains(excluded, buftype) then
-                return
-              else
-                vim.cmd('write')
-              end
-            end
-          end
+          auto_save()
         end
       },
     },
