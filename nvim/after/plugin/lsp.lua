139fbd50ae0ddbb9368834be18357c40334ab822
Bugala Piotr <Piotr.Bugala@igt.com>
2024-08-01 08:25:12 +0200

    feat: Enable JDTLS - java LSP

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -100,6 +100,7 @@ lspconfig.lua_ls.setup(config({
 }))
 lspconfig.golangci_lint_ls.setup(config())
 lspconfig.tsserver.setup(config())
+lspconfig.jdtls.setup(config())
 lspconfig.ansiblels.setup(config({
   settings = {
     ansible = {
