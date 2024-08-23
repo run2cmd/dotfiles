759fb213a2e1d9817cfe9f7efc0adccf988a3a7a
Bugala Piotr <Piotr.Bugala@igt.com>
2024-08-23 08:48:58 +0200

    feat(nvim): Remove cht.sh. Never used to much. Google or ChatGPT is better.

--- nvim/lua/run2cmd/commands.lua
+++ nvim/lua/run2cmd/commands.lua
@@ -1,15 +1,3 @@
 local cmd = vim.api.nvim_create_user_command
 
 cmd('Config', ':e ~/dotfiles/README.md', {})
-
-cmd('Doc',
-  function(params)
-    local buffer = vim.t.doc_window_buffer
-    if buffer and vim.api.nvim_buf_is_valid(buffer) then
-      vim.api.nvim_buf_delete(buffer, {})
-    end
-    vim.cmd('split term://cht.sh ' .. params.args)
-    vim.t.doc_window_buffer = vim.api.nvim_get_current_buf()
-  end,
-  { nargs = '*' }
-)
