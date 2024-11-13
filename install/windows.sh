4b54ca52237ace54c7291ff16803c005f4fc4377
Bugala Piotr <Piotr.Bugala@igt.com>
2024-11-13 12:54:00 +0100

    feat(win): Remove obsolete windows scripts

--- install/windows.sh
+++ install/windows.sh
@@ -6,7 +6,4 @@ source ${libdir}/lib.sh
 repodir=$(dirname $(dirname "$(readlink -f $0)"))
 
 topic 'SETUP WINDOWS TOOLS CONFIGURATION'
-cp ${repodir}/viebrc /mnt/c/Users/${USER}/.viebrc
 cp ${repodir}/wezterm.lua /mnt/c/Users/${USER}/.wezterm.lua
-cp ${repodir}/winbin/* /mnt/c/Users/${USER}/bin/
-cp -rf ${repodir}/vieb /mnt/c/Users/${USER}/
--- winbin/etc-hosts.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start /b "" "gsudo" "notepad" "C:\Windows\System32\drivers\etc\hosts"
