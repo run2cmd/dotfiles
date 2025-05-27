650080ef456e274a3183d8d97c1224caa3d8cb9c
Bugala Piotr <Piotr.Bugala@igt.com>
2025-05-27 12:47:15 +0200

    fix(install): Only install rvm on 1st installation

--- install/ruby.sh
+++ install/ruby.sh
@@ -12,7 +12,7 @@ additional_rubies='2.7.3 2.4.10 2.0.0'
 task 'Update rvm'
 if [ ! -e ${HOME}/.rvm ] ;then
   gpg --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-  curl -sSL https://get.rvm.io | bash -s stable --ruby
+  curl -sSL https://get.rvm.io | bash -s stable
 fi
 
 # shellcheck disable=SC1091
