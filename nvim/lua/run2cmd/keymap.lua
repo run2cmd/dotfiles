6e5eaf0698f2d7e9e06042c292fea640651ba21b
Bugala Piotr <Piotr.Bugala@brightstarlottery.com>
2025-12-03 09:37:38 +0100

    feat: Remove repeaters holdup. Mason or lazy is hard to navigate with that and I already used to not depend on it too much

--- nvim/lua/run2cmd/keymap.lua
+++ nvim/lua/run2cmd/keymap.lua
@@ -1,25 +1,4 @@
 local mapkey = vim.keymap.set
-local helpers = require('run2cmd.helper-functions')
-
-for _, key in ipairs({ "h", "j", "k", "l", "+", "-" }) do
-  local count = 0
-  local timer = assert(vim.loop.new_timer())
-  local map = key
-  vim.keymap.set("n", key, function()
-    if vim.v.count > 0 then
-      count = 0
-    end
-    if count >= 4 then
-      print("Hold on man. Use repeaters.")
-    else
-      count = count + 1
-      timer:start(2000, 0, function()
-        count = 0
-      end)
-      return map
-    end
-  end, { expr = true, silent = true })
-end
 
 mapkey('n', '<silent> <c-l>', ":nohlsearch<C-R>=has('diff')?'<Bar>diffupdate':''<CR><CR><C-L>")
 
