bdaf3e6189282fc1b3224f7907209638a01a3fd2
Bugala Piotr <Piotr.Bugala@brightstarlottery.com>
2025-12-03 09:46:53 +0100

    feat(nvim): Replace alpha with minintro

--- nvim/lua/run2cmd/plugins/alpha.lua
+++ nvim/lua/run2cmd/plugins/alpha.lua
@@ -1,27 +1,7 @@
-local greetme = {
-  [[ ______  _     _  ______ _____      __    _ _______  ______  _   _  _____ _______ ]],
-  [[ |_____] |     | |  ____   |        | \\  | |______ |     |  \\  /    |   |  |  | ]],
-  [[ |_____] |_____| |_____| __|__      |  \\_| |______ |_____|   \\/   __|__ |  |  | ]],
-  [[                                                                                  ]],
-}
-
 return {
   {
-    'goolord/alpha-nvim',
-    config = function()
-      local startup = require('alpha')
-      local theme = require('alpha.themes.startify')
-      local mapkey = vim.keymap.set
-
-      theme.section.header.val = greetme
-      theme.section.mru_cwd.val = { { type = 'padding', val = 0 } }
-      theme.nvim_web_devicons.enabled = false
-
-      startup.setup(theme.config)
-
-      mapkey('n', '<leader>L', ':bufdo %bd | Alpha<CR>:LspRestart<CR>')
-      mapkey('n', '<leader>to', ':tabnew<Bar>Alpha<CR>')
-    end
-  },
-
+    'eoh-bse/minintro.nvim',
+    config = true,
+    lazy = false
+  }
 }
