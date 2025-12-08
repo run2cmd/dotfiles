ff0f570b56d328dcb7fe9da44ecb620ab2d1d28d
Bugala Piotr <Piotr.Bugala@brightstarlottery.com>
2025-12-08 17:27:23 +0100

    fix(nvim): Do not change root directory

--- nvim/lua/run2cmd/vimset.lua
+++ nvim/lua/run2cmd/vimset.lua
@@ -18,6 +18,7 @@ vim.o.spell = false
 vim.o.completeopt = 'menu,menuone,noinsert,noselect'
 vim.o.shortmess = vim.o.shortmess .. 'cm'
 vim.o.complete = string.gsub(vim.o.complete, 't', '')
+vim.o.autochdir = false
 
 vim.o.ignorecase = true
 vim.o.smartcase = true
@@ -50,16 +51,13 @@ vim.g.netrw_banner = 0
 vim.g.netrw_preview = 1
 vim.g.netrw_winsize = 25
 vim.g.netrw_altv = 1
-vim.g.netrw_keepdir = 0
+vim.g.netrw_keepdir = 1
 vim.g.netrw_liststyle = 0
 vim.g.netrw_sizestyle = 'H'
 vim.g.netrw_silent = 1
 vim.g.netrw_special_syntax = 1
 vim.g.netrw_bufsettings = 'noma nomod nu nobl nowrap ro rnu'
 vim.g.netrw_use_errorwindow = 1
-
-vim.g.last_terminal_test = {}
-vim.g.terminal_window_buffer_number = {}
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
